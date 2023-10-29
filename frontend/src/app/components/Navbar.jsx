ae166384b045f8c0fe3a429eb4dd29f65bd93f53
Abhishek G <96347325+GmAbhishek@users.noreply.github.com>
2023-10-29 13:43:58 +0530

    Modified UI

--- frontend/src/app/components/Navbar.jsx
+++ frontend/src/app/components/Navbar.jsx
@@ -7,33 +7,34 @@ export default function Navbar() {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    padding: '1em',
   };
 
   const titleStyle = {
     marginLeft: '10px',
-    color: '#FF69B4', // Hot Pink
+    color: '#00000', // Hot Pink
     fontSize: '2rem',
   };
 
   const buttonStyle = {
     marginRight: '10px',
     padding: '8px',
+    paddingRight: '15px',
+    paddingLeft: '15px',
     borderRadius: '20px',
-    color: '#FF1493', // Deep Pink
+    color: '#00000', // Deep Pink
     fontSize: '1.5rem',
     border: '2px solid #FF69B4', // Hot Pink
     backgroundColor: '#FFC0CB', // Light Pink
     transition: 'background-color 0.3s',
   };
 
-  const buttonHoverStyle = {
-    backgroundColor: '#FF69B4', // Hot Pink (on hover)
-  };
+  
 
   return (
     <div style={navStyle}>
       <div style={titleStyle}>Wanchain-x-CrossChain</div>
-      <button style={buttonStyle} onMouseEnter={() => buttonStyle.backgroundColor = buttonHoverStyle.backgroundColor} onMouseLeave={() => buttonStyle.backgroundColor = navStyle.backgroundColor}>
+      <button style={buttonStyle} >
         Connect
       </button>
     </div>
