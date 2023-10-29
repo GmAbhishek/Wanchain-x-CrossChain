cae965fa915739440b0de9e83020ebcae134a7dd
Gm_Abhishek <gmabhishek2004@gmail.com>
2023-10-29 13:25:50 +0530

    UI modification

--- frontend/src/app/components/Navbar.jsx
+++ frontend/src/app/components/Navbar.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 
 export default function Navbar() {
+  const navStyle = {
+    backgroundColor: '#FFC0CB', // Light Pink
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  };
+
+  const titleStyle = {
+    marginLeft: '10px',
+    color: '#FF69B4', // Hot Pink
+    fontSize: '2rem',
+  };
+
+  const buttonStyle = {
+    marginRight: '10px',
+    padding: '8px',
+    borderRadius: '20px',
+    color: '#FF1493', // Deep Pink
+    fontSize: '1.5rem',
+    border: '2px solid #FF69B4', // Hot Pink
+    backgroundColor: '#FFC0CB', // Light Pink
+    transition: 'background-color 0.3s',
+  };
+
+  const buttonHoverStyle = {
+    backgroundColor: '#FF69B4', // Hot Pink (on hover)
+  };
+
   return (
-    <div className='flex flex-row justify-between items-center bg-primary'>
-        <div className="m-6 ml-10 text-white text-2xl">Wanchain Message</div>
-        <button className="m-6 mr-10 p-2 rounded-xl text-white text-xl border-2 hover:bg-secondary">Connect</button>
+    <div style={navStyle}>
+      <div style={titleStyle}>Wanchain-x-CrossChain</div>
+      <button style={buttonStyle} onMouseEnter={() => buttonStyle.backgroundColor = buttonHoverStyle.backgroundColor} onMouseLeave={() => buttonStyle.backgroundColor = navStyle.backgroundColor}>
+        Connect
+      </button>
     </div>
-  )
+  );
 }
